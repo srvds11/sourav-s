72911ef5a041c7490e22e48704f325a3089917c1
srvds11 <78067634+srvds11@users.noreply.github.com>
2021-01-27 12:08:51 +0530

    portfolio
    My Portfolio

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+	with open('database.txt',mode='a') as database:
+		email=data["email"]
+		subject=data["subject"]
+		message=data["message"]
+		file=database.write(f'\n{email},{subject},{message}')
+
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+	if request.method=='POST':
+		data=request.form.to_dict()
+		write_to_file(data)
+		return 'Thanks !! Form Submitted'
+	else:
+		return 'Something Wrong !!!'
